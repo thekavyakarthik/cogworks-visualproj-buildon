bbacb0544fc0f183ea05043b1bc9b79cd105b6ca
SamGu-NRX <sgu07966@gmail.com>
2024-07-19 09:53:40 -0500

    Co-authored-by: maggie zhu <magizee@users.noreply.github.com> Co-authored-by: dweng37 <dweng37@users.noreply.github.com>

---
+++ descriptor.py
@@ -0,0 +1,32 @@
+import numpy as np
+from cosdistance import find_cos_dist
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+from facenet_models import FacenetModel
+
+model = FacenetModel()
+
+def descriptorMatch(database, descriptor: np.ndarray, threshold: float) -> str:
+    vectorDist = []
+    possiblePeople = []
+    #finding cosine distances
+    for i, d in enumerate(database.values()):
+        dist = find_cos_dist(descriptor, np.mean(d, axis=0))
+        possiblePeople.append(list(database.keys())[i])
+        vectorDist.append(dist)
+    #position of least possible cosine distance
+    pos = np.argmin(vectorDist)
+    if vectorDist[pos] < threshold:
+        #database.add() from Profile class
+        return possiblePeople[pos] #to be used in the final output graph with name
+    return 'No match found'
+
+def displayFinalPicture(array_pic, name):
+    boxes, probabilities, landmarks = model.detect(array_pic)
+    fig, ax = plt.subplots()
+    ax.imshow(array_pic)
+
+    for box, prob, landmark in zip(boxes, probabilities, landmarks):
+        # draw the box on the screen
+        ax.add_patch(Rectangle(box[:2], *(box[2:] - box[:2]), fill=None, lw=2, color="red"))
+        plt.text(box[:2][0], box[:2][1], name, color="red")
